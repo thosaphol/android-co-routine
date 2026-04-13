d86e0da72167e958ed6909328fdf3a0c96ca5a64
thosaphol <thosaphol@outlook.co.th>
2026-04-13 13:23:34 +0700

    Android Coroutines 1/12 intro to coroutine

--- app/src/main/java/com/example/co_routine/MainActivity2.kt
+++ app/src/main/java/com/example/co_routine/MainActivity2.kt
@@ -1,12 +1,19 @@
 package com.example.co_routine
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity2 : AppCompatActivity() {
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,23 @@ class MainActivity2 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        runDemo1()
+    }
+
+
+
+    private fun runDemo1(){
+        GlobalScope.launch {
+            while (true){
+                delay(1000L)
+                //Thread.currentThread().name => ชื่อของ Thread ที่ coroutine ใช้ run
+                Log.d(TAG,"Inside of coroutine ${Thread.currentThread().name}")
+            }
+
+        }
+
+        //Thread.currentThread().name => ชื่อของ Thread ที่ function runDemo1 ใช้ run
+        Log.d(TAG,"Outside of coroutine ${Thread.currentThread().name}")
     }
 }
